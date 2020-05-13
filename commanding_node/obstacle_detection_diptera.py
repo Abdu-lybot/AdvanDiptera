66f498f613fcaca0d7a93bdbbcae4e81ec3ce0c6
rovirolas3 <61912168+rovirolas3@users.noreply.github.com>
2020-05-13 16:10:16 +0200

    Added movements and detections

--- commanding_node/obstacle_detection_diptera.py
+++ commanding_node/obstacle_detection_diptera.py
@@ -1,7 +1,16 @@
+#!/usr/bin/env python
+
+
 import rospy
-from std_msgs.msg import Float32, Float64, String
+from mavros_msgs.srv import CommandBool, CommandTOL, SetMode
 from mavros_msgs.msg import GlobalPositionTarget, State, PositionTarget
-from sensor_msgs.msg import Range
+from geometry_msgs.msg import PoseStamped, Twist
+from sensor_msgs.msg import Imu, NavSatFix
+from pyquaternion import Quaternion
+import time
+import yaml
+from gpio_diptera import Rpi_gpio_comm as Gpio_start
+from gpio_clean import Rpi_gpio_comm_off as Gpio_stop
 
 
 class UltrasonicSensing():
@@ -24,6 +33,12 @@ class UltrasonicSensing():
         subscribed_topic_d = "/sonarTP_D"
         self.down_sensor = rospy.Subscriber(subscribed_topic_d, Range, self.cb_down_sensor)
 
+        self.local_pose_sub = rospy.Subscriber("/mavros/local_position/pose", PoseStamped, self.local_pose_callback)
+        self.imu_sub = rospy.Subscriber("/mavros/imu/data", Imu, self.imu_callback)
+
+        self.local_target_pub = rospy.Publisher('mavros/setpoint_raw/local', PositionTarget, queue_size=10)
+
+
 
     def cb_mavros_state(self, msg):
         self.mavros_state = msg.mode
@@ -37,35 +52,103 @@ class UltrasonicSensing():
 
     def cb_forward_sensor(self, sense):
         if (self.state == "HOVER"):
-            for sense.range in range(0,1):
+            for sense.range not in range(0,1):
                 print("no obstacle close to drone")
             else:
                 #move opposite direction with the difference distance
+                diference = 1-sense.range
+                self.cur_target_pose = self.construct_target(self.local_pose.pose.position.x - diference, self.local_pose.pose.position.y , self.local_pose.pose.position.z, self.current_heading)
+                self.local_target_pub.publish(self.cur_target_pose) 
+                rospy.logwarn("Avoiding Front Obstacle")
 
     def cb_right_sensor(self,sense):
         if (self.state == "HOVER"):
-            for sense.range in range(0,1):
+            for sense.range not in range(0,1):
                 print("no obstacle close to drone")
             else:
                 #move opposite direction with the difference distance
+                diference = 1-sense.range
+                self.cur_target_pose = self.construct_target(self.local_pose.pose.position.x, self.local_pose.pose.position.y + diference, self.local_pose.pose.position.z, self.current_heading)
+                self.local_target_pub.publish(self.cur_target_pose) 
+                rospy.logwarn("Avoiding Right Obstacle") 
 
     def cb_left_sensor(self,sense):
         if (self.state == "HOVER"):
-            for sense.range in range(0,1):
+            for sense.range not in range(0,1):
                 print("no obstacle close to drone")
             else:
                 #move opposite direction with the difference distance
+                diference = 1-sense.range
+                self.cur_target_pose = self.construct_target(self.local_pose.pose.position.x, self.local_pose.pose.position.y - diference, self.local_pose.pose.position.z, self.current_heading)
+                self.local_target_pub.publish(self.cur_target_pose) 
+                rospy.logwarn("Avoiding Left Obstacle")  
 
     def cb_back_sensor(self,sense):
         if (self.state == "HOVER"):
-            for sense.range in range(0,1):
+            for sense.range not in range(0,1):
                 print("no obstacle close to drone")
             else:
                 #move opposite direction with the difference distance
+                diference = 1-sense.range
+                self.cur_target_pose = self.construct_target(self.local_pose.pose.position.x + diference, self.local_pose.pose.position.y, self.local_pose.pose.position.z, self.current_heading)
+                self.local_target_pub.publish(self.cur_target_pose) 
+                rospy.logwarn("Avoiding Back Obstacle")
 
     def cb_down_sensor(self,sense):
         if (self.state == "HOVER"):
-            for sense.range in range(0,1):
+            for sense.range not in range(0,1):
                 print("no obstacle close to drone")
             else:
                 #move opposite direction with the difference distance
+                diference = 1-sense.range
+                self.cur_target_pose = self.construct_target(self.local_pose.pose.position.x, self.local_pose.pose.position.y, self.local_pose.pose.position.z + diference, self.current_heading)
+                self.local_target_pub.publish(self.cur_target_pose) 
+                rospy.logwarn("Avoiding Down Obstacle")
+
+
+    def construct_target(self, x, y, z, yaw, yaw_rate = 1): 
+        target_raw_pose = PositionTarget() # We will fill the following message with our values: http://docs.ros.org/api/mavros_msgs/html/msg/PositionTarget.html
+        target_raw_pose.header.stamp = rospy.Time.now()
+
+        target_raw_pose.coordinate_frame = 9 
+
+        target_raw_pose.position.x = x
+        target_raw_pose.position.y = y
+        target_raw_pose.position.z = z
+
+        target_raw_pose.type_mask = PositionTarget.IGNORE_VX + PositionTarget.IGNORE_VY + PositionTarget.IGNORE_VZ \
+                                    + PositionTarget.IGNORE_AFX + PositionTarget.IGNORE_AFY + PositionTarget.IGNORE_AFZ \
+                                    + PositionTarget.FORCE
+
+        target_raw_pose.yaw = yaw
+        target_raw_pose.yaw_rate = yaw_rate
+
+        return target_raw_pose
+
+    def local_pose_callback(self, msg): 
+        self.local_pose = msg
+        self.local_enu_position = msg
+
+
+    def imu_callback(self, msg):
+
+        self.imu = msg
+
+        self.current_heading = self.q2yaw(self.imu.orientation) # Transforms q into degrees of yaw
+
+        self.received_imu = True
+
+if __name__ == '__main__':
+    for i in range(10): # Waits 5 seconds for initialization
+        if self.current_heading is not None:
+            break
+        else:
+            print("Waiting for initialization.")
+            time.sleep(0.5)
+
+    while rospy.is_shutdown() is False: # While we don't shutdown it, do the loop
+
+            time.sleep(0.1) # Rate 
+
+
+
